edaf4f953907fcbf6c3d7b364adc9051865c2ae7
Won Hong <whong@outlook.com>
2017-10-07 07:03:48 -0400

    mobile design has been completed with modal

--- public/indexPageDesign/landingPageScript.js
+++ public/indexPageDesign/landingPageScript.js
@@ -1,5 +1,16 @@
+function pressedMobileButton() {
+  $(".mobileMenuContainer").css("display","inline-block");
+  $(".searchSection").css("opacity", 0.2);
+}
+
+function closeModalButton() {
+  $(".mobileMenuContainer").css("display","none");
+  $(".searchSection").css("opacity", 1);
+}
+
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     // Do something
     $(".scrollDownArrow").css("display", "none");
 });
+
