ef6ebf490e372fa139d5a8e4895be6e754abcf3e
Won Hong <whong@outlook.com>
2017-10-16 21:56:48 -0400

    turnary operator has been used

--- public/userRegistration/hospitalRecruiters/backend/confirmation.js
+++ public/userRegistration/hospitalRecruiters/backend/confirmation.js
@@ -1,3 +1,26 @@
+var emailContactInfo = {
+  email: "wjh261@stern.nyu.edu",
+  appReceivedNotification: true,
+  interviewScheduledNotification: true,
+  messageReceivedNotification: true,
+  candidateRecommendationNotification: true,
+};
+
+var cellContactInfo = {
+  cell: "3476107626",
+  appReceivedNotification: true,
+  interviewScheduledNotification: true,
+  messageReceivedNotification: true,
+  candidateRecommendationNotification: true,
+};
+
+function selectionButtonClicked(emailOrCell, buttonType) {
+  emailOrCell === "email" ?
+    emailContactInfo[buttonType] = !emailContactInfo[buttonType] :
+    cellContactInfo[buttonType] = !cellContactInfo[buttonType];
+}
+
+
 function sendConfCodeButtonClicked() {
   phoneNumberEntered = $(".phoneConfirmation").val();
   x = phoneNumberEntered.replace(/-/g, '');
@@ -8,8 +31,6 @@ function sendConfCodeButtonClicked() {
   $(".submitCodeButton").css("display", "inline");
   $(".sendCodeButton").css("display", "inline");
 }
-
-
 
 $( document ).ready(function() {
   setTimeout(function(){
