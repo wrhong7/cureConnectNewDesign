5919c4be657368d60bce45b33a7f6f4ebefa1761
wrhong7 <whong@outlook.com>
2017-11-04 11:54:14 -0400

    search functionality is half complete

--- public/search/Results.js
+++ public/search/Results.js
@@ -1,11 +1,26 @@
 var professionalFields = ["doctor", "nurse", "recruiter", "psychologist", "physical therapists"];
-var zipCodesFetched = [];
 var zipCodeRequestURL;
+var neighboringZipCodesOfZipEntered = [];
+var zipCodeListingFromDB;
+
+//we need to assign this on cache. If there is nothing entered on cache variable, we should fetch nothing and tell
+//users to answer the job type and zip code. we should log every search results.
+var zipCodeEnteredFromPreviousPage = "10003";
+
+// for instance from the pervious search page, the user should have entered the job catergory along with the zipcode.
+// there should be two scenarios. 1) there should be one situation where zipcode is available.
+// and 2) if zipcode is not entered, we should say, please enter the zipcode for the search.
+// if the area that we serve has not been entered, then we say, sorry, we are not available in your area yet.
+
+function nothingEnteredOnSearchQuery() {
+  $(".jobSearchResults").empty();
+  $(".jobSearchResults").append('<div class="searchCriteriaQuery">Please enter the search criteria for results</div>');
+}
 
 function fetchSearchResults() {
   searchPosition = $("#professionalFields").val();
-  searchZipCode = $(".searchZipcodeInputBox").val();
-  pullAllNeighboringZipCode(searchZipCode);
+  // zipCodeEnteredFromPreviousPage = $(".searchZipcodeInputBox").val();
+  pullAllNeighboringZipCode(zipCodeEnteredFromPreviousPage);
 }
 
 function pullAllNeighboringZipCode(zipCode) {
@@ -14,9 +29,90 @@ function pullAllNeighboringZipCode(zipCode) {
   $.ajax({url: zipCodeRequestURL, success: function(result){
       result = result["zip_codes"];
       zipCodeArray = result;
-      //   //sorting zipcodes to fetch it by the closest distance
-      zipCodeArray.sort(function(first,second) {return first["distance"]-second["distance"]});
+      // //sorts zipcode array based on distance
+      // zipCodeArray.sort(function(first,second) {return first["distance"]-second["distance"]});
+      zipCodeArray.forEach(function(zip) {
+        neighboringZipCodesOfZipEntered.push(zip["zip_code"]);
+      })
+
+      compareEnteredZipCodesAgainstServerZipCodes();
+
+      // zipCodeArray.forEach(function(zip){
+      //   console.log(zip["zip_code"]);
+      // });
   }});
+}
+
+function pullZipCodeEntered() {
+  if (zipCodeEnteredFromPreviousPage != null) {
+    if (zipCodeEnteredFromPreviousPage == "") {
+      nothingEnteredOnSearchQuery();
+    } else {    
+      fetchSearchResults();
+    }
+  } else if (zipCodeEnteredFromPreviousPage == null) {
+    nothingEnteredOnSearchQuery();
+  }
+}
+
+function compareEnteredZipCodesAgainstServerZipCodes() {
+  zipCodesToBeFetched = zipCodeListingFromDB.filter( function(n) { 
+    return this.has(n) 
+  }, new Set(neighboringZipCodesOfZipEntered) );
+  loadAllJobsUnderSearchingZipCode();
+}
+
+function loadAllJobsUnderSearchingZipCode() {
+  zipCodesToBeFetched.forEach(function(zip) {
+    console.log(zip);
+
+    //need to fetch all the jobs listed under the zipcode
+    jobDB = firebaseDB.ref('jobsDB/'+zip);
+    jobDB.on('value',function(data){
+      jobs = data.val();
+      listOfJobsToBeAdded = Object.keys(jobs);
+      listOfJobsToBeAdded.forEach(function(x) {
+        console.log(jobs[x]);
+
+        //when this thing gets clicked, we need to pass onto the zipcode and object key used for the details section
+        //fetching
+
+        $(".jobSearchResults").append(
+          `     
+          <div class="jobPostingCover">
+            <div class="upperFloor">
+              <div class="jobTitle">
+                Nurse Practitioner - Urgent Care
+              </div>
+              <div class="addToFavorites">
+                +
+              </div>
+            </div>
+
+            <div class="lowerFloor">
+              <div class="lowerFloorLeft">
+                <div class="employerName">
+                  Barton Associates
+                </div>
+                <div class="location">
+                  Niagara Falls, NY
+                </div>
+              </div>
+              <div class="lowerFloorRight">
+                <div class="compRange">
+                  $45-$55/hr
+                </div>
+                <div class="location">
+                  20 hr/wk
+                </div>
+              </div>
+            </div>
+          </div>
+          `
+        )
+      });
+    });
+  });
 }
 
 $(document).ready(function() {
@@ -32,23 +128,34 @@ $(document).ready(function() {
     )
   })
 
-  //this is a bug this part needs to be fixed
-  fetchSearchResults();
-
   firebase.initializeApp(config);
   firebaseDB = firebase.database();
-  neighboringZipcodes.forEach(function(individualZipCode) {
-    jobPostingData = firebase.database().ref('jobsDB/'+individualZipCode);
-    jobPostingData.on('value', function(data) {
-      var keyArray = Object.keys(data.val());
-        keyArray.forEach(function(individualKey) {
-          jobPostingData = firebase.database().ref('jobsDB/'+individualZipCode+"/"+individualKey);
-          jobPostingData.on('value', function(data) {
-            console.log(data.val());
-          })
-        })
-    })
+  zipCodeDB = firebaseDB.ref('jobsDB/zipcodes');
+  zipCodeDB.on('value',function(data){
+    zipCodeListingFromDB = data.val();
   })
+
+  setTimeout(function() {
+
+
+    pullZipCodeEntered();
+
+
+  }, 2000)
+
+
+  // zipCodeArray.forEach(function(individualZipCode) {
+  //   jobPostingData = firebase.database().ref('jobsDB/'+individualZipCode);
+  //   jobPostingData.on('value', function(data) {
+  //     var keyArray = Object.keys(data.val());
+  //       keyArray.forEach(function(individualKey) {
+  //         jobPostingData = firebase.database().ref('jobsDB/'+individualZipCode+"/"+individualKey);
+  //         jobPostingData.on('value', function(data) {
+  //           console.log(data.val());
+  //         })
+  //       })
+  //   })
+  // })
 
   // jobPostingData = firebase.database().ref('jobsDB/zipcodes');
   // jobPostingData.on('value', function(data) {
@@ -112,3 +219,9 @@ $(document).ready(function() {
   //
 	// }
 });
+
+
+
+
+
+
